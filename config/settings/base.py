5e6196ee745e02c714d381a4822fe111783f58aa
Scot Hacker <shacker@birdhouse.org>
2017-11-11 17:48:24 -0800

    Move users app into top-level python space

--- config/settings/base.py
+++ config/settings/base.py
@@ -245,8 +245,8 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
-ACCOUNT_ADAPTER = 'tangelo.users.adapters.AccountAdapter'
-SOCIALACCOUNT_ADAPTER = 'tangelo.users.adapters.SocialAccountAdapter'
+ACCOUNT_ADAPTER = 'users.adapters.AccountAdapter'
+SOCIALACCOUNT_ADAPTER = 'users.adapters.SocialAccountAdapter'
 
 # Custom user app defaults
 # Select the correct user model
--- users/apps.py
+++ users/apps.py
@@ -2,6 +2,4 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    name = 'tangelo.users'
-    # name = 'users'
     verbose_name = "Users"
